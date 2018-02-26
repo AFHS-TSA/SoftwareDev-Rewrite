1a09f1d6e4acf2fd0fef50e0bd041d1ec6c62c37
Jason Long <jasonlongball@gmail.com>
2018-02-25 19:36:48 -0500

    Base application started, created a minimum size listener

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,11 @@
 package main.java.app;
 	
+import java.util.concurrent.TimeUnit;
+
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
@@ -12,10 +16,34 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("/main/resources/app/view/Base.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("/main/resources/app/css/application.css").toExternalForm());
+			primaryStage.initStyle(StageStyle.UNIFIED);
+		   	primaryStage.setHeight(Var.height);
+			primaryStage.setWidth(Var.width);
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			// Listener for minimum width and height 
+			TimeUnit.SECONDS.sleep((long) .1);
+			ChangeListener<Number> stageSizeListener = (observable, oldValue, newValue) -> {
+		    	System.out.println("Height: " + primaryStage.getHeight() + " Width: " + primaryStage.getWidth());
+				if(primaryStage.getHeight() == Var.height && primaryStage.getWidth() == Var.width) {
+				   	primaryStage.setHeight(Var.height);
+					primaryStage.setWidth(Var.width);
+				} else {
+					if(primaryStage.getHeight() < Var.minHeight) { 
+						primaryStage.setHeight(Var.minHeight); 
+					} else if(primaryStage.getWidth() < Var.minWidth) { 
+						primaryStage.setWidth(Var.minWidth); 
+					} else { 
+						primaryStage.setResizable(true);
+					}
+				}
+			};		    
+		    primaryStage.widthProperty().addListener(stageSizeListener);
+		   	primaryStage.heightProperty().addListener(stageSizeListener);
+		
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
---
+++ src/main/java/app/Var.java
@@ -0,0 +1,10 @@
+package main.java.app;
+
+public class Var {
+	
+	// Base Frame width and height props
+	public static int width = 1500;
+	public static int height = 1000;
+	public static int minWidth = 200;
+	public static int minHeight = 200;
+}
