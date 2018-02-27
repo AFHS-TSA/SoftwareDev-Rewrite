8fdcba63c7ffbbbeefc9c638747e9dfc7323515a
Jason Long <jasonlongball@gmail.com>
2018-02-26 21:20:57 -0500

    adding drawer layout and more resizing control

--- src/main/java/app/controllers/BaseController.java
+++ src/main/java/app/controllers/BaseController.java
@@ -1,5 +1,58 @@
 package main.java.app.controllers;
 
-public class BaseController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.effects.JFXDepthManager;
+import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class BaseController implements Initializable {
+
+	@FXML
+	private JFXDrawer leftNavDrawer;
+	@FXML
+	private JFXHamburger navBurger;
+	
+    private void Drawer() {
+        try {    	
+    		AnchorPane drawerContent = FXMLLoader.load(getClass().getResource("/main/resources/app/view/DrawerContent.fxml"));
+    		leftNavDrawer.setSidePane(drawerContent);
+        	HamburgerBackArrowBasicTransition burgerTask = new HamburgerBackArrowBasicTransition(navBurger);
+        	burgerTask.setRate(-1);
+        	navBurger.addEventHandler(MouseEvent.MOUSE_PRESSED, (e)->{
+        	    burgerTask.setRate(burgerTask.getRate()*-1);
+        	    burgerTask.play();
+        	    
+        	    if(leftNavDrawer.isShown()) {
+        	    	leftNavDrawer.close();
+        	    } else {
+        	    	leftNavDrawer.open();
+        	    }
+        	 });
+        	
+        } catch (IOException e1) {
+    		e1.printStackTrace();
+        }
+    }
+    
+    @SuppressWarnings("static-access")
+	private void Shadow() {
+    	JFXDepthManager shadow = null;
+    	//shadow.setDepth(topBar, Variables.shadow);
+    }
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		Drawer();
+	}
 	
 }
